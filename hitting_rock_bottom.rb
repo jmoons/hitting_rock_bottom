7e356efea2f7e9b4d3b12c0e1317cc9ef7078f78
John Moons <j.moons@realdigitalmedia.com>
2013-02-14 18:05:19 -0500

    Updated Readme and did away with hash representation of each row because:
    1) tracking depth here makes no sense
    2) only need a array representation of a row's map data.

--- hitting_rock_bottom.rb
+++ hitting_rock_bottom.rb
@@ -4,14 +4,14 @@ class HittingRockBottom
 	def initialize(args)
 		@input_file = parse_input_file(args)
 		@total_water_units = File.open(args).readline
-		
+
 	end
 
 	def parse_input_file(input_file)
 		parsed_input_file = []
 		File.open(input_file).each_line do |line|
 			next unless line.start_with?("#") || line.start_with?("~")
-			parsed_input_file << {:depth => 0, :map => line.chomp.split(//)}
+			parsed_input_file << line.chomp.split(//)
 		end
 		parsed_input_file
 	end
