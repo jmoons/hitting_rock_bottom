592c9642a1e67242f4862d3df68c59c6eaed115f
John Moons <j.moons@realdigitalmedia.com>
2013-02-15 17:11:07 -0500

    Updated README. First cut at water flow, though it is broken

--- hitting_rock_bottom.rb
+++ hitting_rock_bottom.rb
@@ -2,9 +2,39 @@
 class HittingRockBottom
 	
 	def initialize(args)
-		@input_file = parse_input_file(args)
-		@total_water_units = File.open(args).readline
+		@cave_map		 						= parse_input_file(args)
+		@total_water_units 			= File.open(args).readline
+		@volume_of_each_column 	= initialize_volume
 
+	end
+
+	def pump_water
+		(1..@total_water_units.to_i).each do |water_unit|
+			water_placed = false
+			@cave_map.reverse!.each do |cave_map_row|
+				break if water_placed
+				cave_map_row.each_with_index do |cave_map_row_item, index|
+					break if water_placed
+					if cave_map_row_item == " "
+						cave_map_row[index] = "~"
+						water_placed = true
+					end
+				end
+			end
+		end
+		@cave_map
+	end
+
+	def print_map
+		@cave_map.each{|row| puts row.inspect + "\n"}
+	end
+
+	private
+
+	def initialize_volume
+		volume = []
+		(1..@cave_map.first.length).each{|column| volume << 0}
+		volume
 	end
 
 	def parse_input_file(input_file)
@@ -19,4 +49,7 @@ class HittingRockBottom
 	
 end
 
-HittingRockBottom.new("PuzzleNodeFiles/simple_cave.txt")
+# HittingRockBottom.new("PuzzleNodeFiles/simple_cave.txt").pump_water
+test = HittingRockBottom.new("PuzzleNodeFiles/simple_cave.txt")
+test.pump_water
+test.print_map
